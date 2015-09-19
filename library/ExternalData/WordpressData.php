bd5d4ec3b427b2ef017e53e1d9a347f24f3a41b9
Rishi Satsangi <rishi.satsangi@gmail.com>
2015-09-18 21:20:53 -0400

    updated wordpress class

--- library/ExternalData/WordpressData.php
+++ library/ExternalData/WordpressData.php
@@ -16,11 +16,11 @@ class WordpressData extends DataBase {
         if(!$cache) {
             // echo $cacheKey; die();
             foreach ($ids as $id) {
-                $response = $this->httpClient->get('http://blog.rishisatsangi.com/wp-json/wp/v2/posts/' . $id)->send();
+                $response = $this->httpClient->get('http://fearlessbeautyblog.artistcontrolbox.com/wp-json/wp/v2/posts/' . $id)->send();
                 $response = json_decode($response->getBody(true));
 
                 if($response->featured_image > 0) {
-                    $mediaResponse = $this->httpClient->get('http://blog.rishisatsangi.com/wp-json/wp/v2/media/' . $response->featured_image)->send();
+                    $mediaResponse = $this->httpClient->get('http://fearlessbeautyblog.artistcontrolbox.com/wp-json/wp/v2/media/' . $response->featured_image)->send();
                     $response->featured_image = json_decode($mediaResponse->getBody(true));
                 }
 
